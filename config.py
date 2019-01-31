966aa91b1559371b60382f75705e028a0650fbb2
hippoandy <ycaho@ucdavis.edu>
2019-01-31 10:25:31 -0300

    General settings and parameters

---
+++ config.py
@@ -0,0 +1,17 @@
+
+
+path_data = './data/'
+path_img = 'imgs/'
+path_countries = './countries.txt'
+
+base_url = 'https://www.freelancer.com/'
+
+# selenium settings ----------------------------------------
+f_cookie = 'cookies.pkl'
+
+concurrent = 5
+
+sleep_short = 1
+sleep_med = 3
+sleep_long = 10
+# ---------------------------------------- selenium settings
