03d795fdf2e0de3b752ab128c8c26853cceef3ed
hippoandy <ycaho@ucdavs.edu>
2019-02-08 12:22:15 -0300

    Increase num. of concurrent

--- classifing/label_image.py
+++ classifing/label_image.py
@@ -42,7 +42,7 @@ input_std = 255
 input_layer = "input"
 output_layer = "InceptionV3/Predictions/Reshape_1"
 
-concurrent = 10
+concurrent = 50
 
 labels = []
 # ------------------------------------------------------------ parameter settings
