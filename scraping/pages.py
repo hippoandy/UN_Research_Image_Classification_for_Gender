ef99975c0e3f4479dc37c0dda688d091c7a6e753
hippoandy <hippo1993928@gmail.com>
2019-02-12 20:56:57 -0300

    Change lib import states

--- scraping/pages.py
+++ scraping/pages.py
@@ -1,23 +1,21 @@
 # from selenium import webdriver
 from seleniumrequests import Chrome
+from bs4 import BeautifulSoup
 
 import pickle
 import wget
 import os, time
-
 import json
-
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-from bs4 import BeautifulSoup
+from utilsDAWS import ops_data as ops
+from utilsDAWS import ops_file as rw
 
 import sys
 sys.path.append( '..' )
 import config
-
-from utilsDAWS import ops_data, ops_thread, ops_file
 
 # url = config.base_url + 'freelancers/{}/all/{}'
 url = config.base_url + 'search/users/any_skill/{}/{}/'
@@ -32,8 +30,8 @@ if __name__ == '__main__':
 
         for c in countries:
             urls = []
-            c = ops_data.clean_str( c )
+            c = ops.clean_str( c )
             for i in range( 1, limit ):
                 c = c.replace( ' ', '_' )
                 urls.append( url.format( c.lower(), i ) )
-            ops_file.write_to_json( r'{}{}'.format( config.path_data, f_result.format( c ) ), urls )
+            rw.write_to_json( r'{}{}'.format( config.path_data, f_result.format( c ) ), urls )
